cfe84ff82bbb285cacc0fe00130f64f7144eb3f7
MaDud <martaduda@interia.eu>
2021-06-12 09:19:52 +0200

    Create basic style for rommBox

--- components/Room.js
+++ components/Room.js
@@ -3,17 +3,34 @@ import {StyleSheet, View, Image, Text} from 'react-native';
 
 const Room = ({name, source}) => {
      return (
-         <View>
-            <View >
-                <Image source={
-                    source ? {uri: source} : require('../assets/favicon.png')
-                }/>
-            </View>
+         <View style={styles.roomBox}>
+            <Image source={source ? {uri: source} : require('../assets/favicon.png')}
+                style={styles.profileImg}
+            />
             <View >
                  <Text>{name}</Text>
             </View>
          </View>
      )
 };
+
+const styles = StyleSheet.create( {
+    roomBox: {
+        display: 'flex',
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        paddingTop: 12,
+        paddingRight: 16,
+        paddingBottom: 12,
+        paddingLeft: 16,
+        marginTop: 20
+    },
+    profileImg: {
+        height: 64,
+        width: 64,
+        marginRight: 16
+    }
+})
 
 export default Room
