ab999dbfd4be0dfe945f62274efa28eb8adceeeb
MaDud <martaduda@interia.eu>
2021-06-12 13:33:41 +0200

    Implement input without sending message

--- components/ChatRoom.js
+++ components/ChatRoom.js
@@ -1,36 +1,79 @@
-import React from 'react';
-import { Pressable, View, TextInput } from 'react-native';
-import Header from './Header';
+import React, {useState} from 'react';
+import { StyleSheet, Pressable, View, TextInput, Image, Text } from 'react-native';
 import {GET_CHAT_DATA} from '../index';
-import {useQuery} from '@apollo/client'
+import {useQuery} from '@apollo/client';
+import Header from './Header';
 
 const ChatRoom = () => {
     const {loading, data} = useQuery(GET_CHAT_DATA, {
         variables: {id: "93d14fbd-dfc7-410b-b063-052c89fdd24f"}
     });
+    const [text, setText] = useState('hej');
 
     if (loading) {
-        console.log('loading')
-    } else {
-        console.log(data)
-    }
+        return <Text>Loading...</Text>
+    } 
 
     return (
         <View>
             <Header>
-
+                <View style={styles.chatHeader}>
+                    <Image source={require('../assets/favicon.png')} style={styles.chatIcon}/>
+                    <View>
+                        <Text>{data.room.name}</Text>
+                        <Text></Text>
+                    </View>
+                </View>
             </Header>
             <View>
 
             </View>
-            <View>
-                <TextInput />
+            <View style={styles.newMessageBox}>
+                <TextInput
+                    style={styles.input}
+                    defaultValue={text}
+                    onChangeText={text => setText(text)}
+                />
                 <Pressable>
-
+                    <Text>send</Text>
                 </Pressable>
             </View>
         </View>
     )
 };
+
+const styles = StyleSheet.create( {
+    chatHeader: {
+        display: "flex",
+        flexDirection: "row"
+    },
+    chatIcon: {
+        width: 44,
+        height: 44,
+        borderRadius: 50
+    },
+    newMessageBox: {
+        position: "fixed",
+        bottom: 0,
+        backgroundColor: "#B6DEFD",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingTop: 15,
+        paddingBottom: 15,
+        width: '100%',
+        height: 68,
+        borderTopLeftRadius: 12,
+        borderTopRightRadius: 12
+    },
+    input: {
+        backgroundColor: "#fff",
+        flex: 1,
+        borderTopLeftRadius: 12,
+        borderTopRightRadius: 12,
+        borderBottomLeftRadius: 12,
+        marginLeft: 16,
+    }
+})
 
 export default ChatRoom
