e31d13676765135b32e4a09879507e0063664491
MaDud <martaduda@interia.eu>
2021-06-12 08:50:22 +0200

    Create basic header

---
+++ components/Header.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {StyleSheet, View, Pressable, Text} from 'react-native';
+
+const Header = ({children}) => {
+    return (
+        <View style={styles.headerBgColor}>
+            <View>
+                {children}
+            </View>
+            <View style={styles.buttonsBox}>
+               <Pressable style={styles.iconBtn}>
+                    <Text>search</Text>
+               </Pressable>
+               <Pressable style={styles.iconBtn}>
+                    <Text>users</Text>
+               </Pressable>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    headerBgColor: {
+        backgroundColor: "#B6DEFD",
+        paddingTop: 60,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20
+    },
+    buttonsBox: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    iconBtn: {
+        // width: 44,
+        padding: 10,
+        borderRadius: 50,
+        backgroundColor: "#fff"
+    },
+    icon: {
+        width: 100
+    }
+})
+
+export default Header;
