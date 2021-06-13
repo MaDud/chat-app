a1f29f8f65834799206e949a9ac7ed9c0839e4ec
MaDud <martaduda@interia.eu>
2021-06-13 17:47:47 +0200

    Try to implement wss

--- App.js
+++ App.js
@@ -4,9 +4,13 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  createHttpLink
+  createHttpLink,
+  split
 } from "@apollo/client";
 import { setContext } from '@apollo/client/link/context';
+import * as AbsintheSocket from "@absinthe/socket";
+import { createAbsintheSocketLink } from "@absinthe/socket-apollo-link";
+import { Socket as PhoenixSocket } from "phoenix";
 import { StyleSheet, View } from 'react-native';
 import {useFonts, Poppins_400Regular} from '@expo-google-fonts/poppins';
 import AppLoading from 'expo-app-loading';
@@ -14,24 +18,49 @@ import UsersRooms from './components/UsersRooms';
 import ChatRoom from './components/ChatRoom';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Cookies from "js-cookie";
 
-const token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJjaGF0bHkiLCJleHAiOjE2MjQ1Mzg4NTQsImlhdCI6MTYyMjExOTY1NCwiaXNzIjoiY2hhdGx5IiwianRpIjoiZTBlODM3NDgtODkxZS00ZmQyLWFmNjUtYmE5MjlhYmUzOWUwIiwibmJmIjoxNjIyMTE5NjUzLCJzdWIiOiI0ODU2ODVmMS1mNGQxLTQzOWYtYWE3MC0zYjg1ZWI4NjZmZTkiLCJ0eXAiOiJhY2Nlc3MifQ.JQy40M5jYX0ZRuEeItXUsHrHwEyJHXzSXdGi1hV6HHax9FJB9D2FjqX3fBzteUkLApFtHOcGVwUADpFp-tbUfw"
+const myToken = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJjaGF0bHkiLCJleHAiOjE2MjQ1Mzg4NTQsImlhdCI6MTYyMjExOTY1NCwiaXNzIjoiY2hhdGx5IiwianRpIjoiZTBlODM3NDgtODkxZS00ZmQyLWFmNjUtYmE5MjlhYmUzOWUwIiwibmJmIjoxNjIyMTE5NjUzLCJzdWIiOiI0ODU2ODVmMS1mNGQxLTQzOWYtYWE3MC0zYjg1ZWI4NjZmZTkiLCJ0eXAiOiJhY2Nlc3MifQ.JQy40M5jYX0ZRuEeItXUsHrHwEyJHXzSXdGi1hV6HHax9FJB9D2FjqX3fBzteUkLApFtHOcGVwUADpFp-tbUfw"
 
 const httpLink = createHttpLink( {
   uri: 'https://chat.thewidlarzgroup.com/api/graphql'
 });
 
 const authLink = setContext((_, { headers }) => {
+  const token = Cookies.get("token");
   return {
     headers: {
       ...headers,
-      authorization: `Bearer ${token}`,
+      authorization: token ? `Bearer ${myToken}` : "",
     }
   }
 });
 
+const authedHttpLink = authLink.concat(httpLink);
+
+const phoenixSocket = new PhoenixSocket("ws://localhost:4000/socket", {
+  params: () => {
+    if (Cookies.get("token")) {
+      return { token: Cookies.get("token") };
+    } else {
+      return {token: myToken}
+      };
+    }
+});
+
+const absintheSocket = AbsintheSocket.create(phoenixSocket);
+
+const websocketLink = createAbsintheSocketLink(absintheSocket);
+
+const link = split(
+  operation => hasSubscription(operation.query),
+  websocketLink,
+  authedHttpLink
+);
+
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: link,
   cache: new InMemoryCache()
 });
 
